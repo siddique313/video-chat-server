d8c6172ba78559726b45f7dd4a4d5a03b3ed92ad
saddiqueali <siddiqueali531@gmail.com>
2026-02-26 03:05:25 +0500

    wip

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT = process.env.PORT || 8000;
+export const PORT = process.env.PORT || 8080;
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || "*";
