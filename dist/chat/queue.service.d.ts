dd9203d468296730a05bc68c25d42b048719e9a9
siddique313 <siddiqueali531@gmail.com>
2026-04-20 23:45:38 +0500

    Remove dist/ from tracking

--- dist/chat/queue.service.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class QueueService {
-    private items;
-    enqueue(id: string): void;
-    dequeue(): string | undefined;
-    remove(id: string): void;
-    has(id: string): boolean;
-    size(): number;
-    print(): void;
-}
